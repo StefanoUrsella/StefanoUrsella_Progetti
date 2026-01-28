0763215b09c2600a4784fbe79fa20dbad2f6f8ab
Ursella Stefano <stefano.ursella@cz.lan>
2026-01-28 13:46:25 +0100

    28/01/2026

--- codice.js
+++ codice.js
@@ -1,12 +1,36 @@
 class Truppa{
 
-    constructor(){
-        this.inputHP = document.getElementById("inputHP1");
-        this.inputDANNO = document.getElementById("inputDANNO1");
-        this.inputDIFESA = document.getElementById("inputDIFESA1");
-        this.inputATTACCO = document.getElementById("inputATTACCO1");
-        this.inputATTACCHI = document.getElementById("inputATTACCHI1");
-        this.inputMORALE = document.getElementById("inputMORALE1");
+    truppaFazione;
+
+    constructor(truppaFazione){
+        this.truppaFazione = truppaFazione;
+
+        if(truppaFazione == 1){
+            this.inputHP = document.getElementById("inputHP1");
+            this.inputDANNO = document.getElementById("inputDANNO1");
+            this.inputDIFESA = document.getElementById("inputDIFESA1");
+            this.inputATTACCO = document.getElementById("inputATTACCO1");
+            this.inputATTACCHI = document.getElementById("inputATTACCHI1");
+            this.inputMORALE = document.getElementById("inputMORALE1");
+            this.inputTREUPPEINIZIALI = document.getElementById("inputTruppeIniziali1");
+            this.inputTRUPPE = document.getElementById("inputTRUPPE1");
+            this.displayMORALERIMASTO = document.getElementById("moraleRimasto1");
+        }else if(truppaFazione == 2){
+            this.inputHP = document.getElementById("inputHP2");
+            this.inputDANNO = document.getElementById("inputDANNO2");
+            this.inputDIFESA = document.getElementById("inputDIFESA2");
+            this.inputATTACCO = document.getElementById("inputATTACCO2");
+            this.inputATTACCHI = document.getElementById("inputATTACCHI2");
+            this.inputMORALE = document.getElementById("inputMORALE2");
+            this.inputTREUPPEINIZIALI = document.getElementById("inputTruppeIniziali2");
+            this.inputTRUPPE = document.getElementById("inputTRUPPE2");
+            this.displayMORALERIMASTO = document.getElementById("moraleRimasto2");
+        }
+    }
+
+    aggiornareMorale(){
+        //this.moraleMassimo = Number(this.inputTREUPPEINIZIALI.value) * Number(this.inputMORALE.value);
+        this.displayMORALERIMASTO.textContent = Math.round(Number(this.inputMORALE.value) * Number(this.inputTRUPPE.value) / Number(this.inputTREUPPEINIZIALI.value));
     }
 
     getHP(hp){
@@ -18,7 +42,7 @@ class Truppa{
     }
 
     getDANNO(danno){
-        this.inputDANNO.value = DANNO;
+        this.inputDANNO.value = danno;
     }
 
     setDANNO(){
@@ -57,19 +81,66 @@ class Codice{
     constructor(){
         this.buttonRissa = document.getElementById("buttonRissa");
         this.buttonRissa.addEventListener("click", () => this.onButtonClick());
-        this.truppa1 = new Truppa();
-        this.truppa2 = new Truppa();
+        this.truppa1 = new Truppa(1);
+        this.truppa2 = new Truppa(2);
     }
 
     onButtonClick(){
         console.log("pulsnte cliccato");
-        this.stampareInput();
+        this.stampareInput(this.truppa1);
+        this.stampareInput(this.truppa2);
+        this.truppa1.aggiornareMorale();
+        this.truppa2.aggiornareMorale();
+        lanciaDadi(5);
     }
 
-    stampareInput(){
-        console.log("HP: " + this.inputHP.value + "\nDANNO: " + this.inputDANNO.value + "\nDIFESA: " + this.inputDIFESA.value + "\nATTACCO: " + this.inputATTACCO.value + "\nTRUPPE: " + this.inputTRUPPE.value + "\nMORALE: " + this.inputMORALE.value);
+    stampareInput(truppa){
+        console.log("HP: " + truppa.inputHP.value + "\nDANNO: " + truppa.inputDANNO.value + "\nDIFESA: " + truppa.inputDIFESA.value + "\nATTACCO: " + truppa.inputATTACCO.value + "\nATTACCHI: " + truppa.inputATTACCHI.value + "\nMORALE: " + truppa.inputMORALE.value);
     }
 }
 
 //Per creare la classe, sennò non c'è nulla che la crea
 const codice = new Codice();
+
+
+
+
+//DADI
+function lanciaDadi(numeroDadi, durataAnimazione = 1000, durataVisibile = 2000) {
+    const contenitoreDadi = document.getElementById("contenitoreDadi");
+    contenitoreDadi.innerHTML = ""; //cancella il contenuto di "contenitoreDadi"
+
+    const dadi = [];
+
+    // 1. Creazione dei dadi
+    for (let i = 0; i < numeroDadi; i++) {
+        const dado = document.createElement("div"); //"dado" è un nuovo elemento "generico" (è un "div")
+        dado.classList.add("dadoAttacco"); //li dico che classe CSS deve usare il dado
+        dado.textContent = "-";
+        contenitoreDadi.appendChild(dado);
+        dadi.push(dado);//aggiungo i "dado" alla lista di "dadi"
+    }
+
+    // 2. Animazione casuale veloce
+    const intervalli = dadi.map(dado => {//"intervalli" contiene tutti i "setInterval" di ogni dado             //"map" esegue un metodo su tutti gli elementi di un array
+        return setInterval(() => {//"setInterval" esegue una funzione ogni TOT tempo
+            dado.textContent = Math.floor(Math.random() * 6) + 1;
+        }, 50); //"50" è la velocità con cui viene iterato "setInterval"
+    });
+
+    // 3. Stop animazione dopo X ms
+    setTimeout(() => {
+        intervalli.forEach(interval => clearInterval(interval));
+
+        // Risultato finale
+        dadi.forEach(dado => {
+            dado.textContent = Math.floor(Math.random() * 6) + 1;
+        });
+
+        // 4. Scomparsa dopo Y ms
+        setTimeout(() => {
+            contenitoreDadi.innerHTML = "";
+        }, durataVisibile);
+
+    }, durataAnimazione);
+}
