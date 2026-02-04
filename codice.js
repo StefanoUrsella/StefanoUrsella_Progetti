96fff6b23aa9cd58c8530c56522eb93b799a77c9
Ursella Stefano <stefano.ursella@cz.lan>
2026-02-04 13:42:55 +0100

    04/02/2026

--- codice.js
+++ codice.js
@@ -5,6 +5,11 @@ class Truppa{
     constructor(truppaFazione){
         this.truppaFazione = truppaFazione;
 
+        this.tiriAttacco1 = [];
+        this.tiriDifesa1 = [];
+        this.tiriAttacco2 = [];
+        this.tiriDifesa2 = [];
+
         if(truppaFazione == 1){
             this.inputHP = document.getElementById("inputHP1");
             this.inputDANNO = document.getElementById("inputDANNO1");
@@ -103,80 +108,119 @@ class Codice{
         this.modificatoreAttacco2.textContent = this.truppa2.inputATTACCO.value;
         this.modificatoreDifesa2.textContent = this.truppa1.inputDIFESA.value;
 
-        lanciaDadi((Number(this.truppa1.inputTRUPPE.value)*Number(this.truppa1.inputATTACCHI.value)), "attacco1");
-        lanciaDadi((Number(this.truppa1.inputTRUPPE.value)*Number(this.truppa1.inputATTACCHI.value)), "difesa1");
-        lanciaDadi((Number(this.truppa2.inputTRUPPE.value)*Number(this.truppa2.inputATTACCHI.value)), "attacco2");
-        lanciaDadi((Number(this.truppa2.inputTRUPPE.value)*Number(this.truppa2.inputATTACCHI.value)), "difesa2");
+        this.tiriAttacco1 = [];
+        this.tiriDifesa1 = [];
+        this.tiriAttacco2 = [];
+        this.tiriDifesa2 = [];
+
+        this.lanciaDadi((Number(this.truppa1.inputTRUPPE.value)*Number(this.truppa1.inputATTACCHI.value)), "attacco1");
+        this.lanciaDadi((Number(this.truppa1.inputTRUPPE.value)*Number(this.truppa1.inputATTACCHI.value)), "difesa1");
+        this.lanciaDadi((Number(this.truppa2.inputTRUPPE.value)*Number(this.truppa2.inputATTACCHI.value)), "attacco2");
+        this.lanciaDadi((Number(this.truppa2.inputTRUPPE.value)*Number(this.truppa2.inputATTACCHI.value)), "difesa2");
+
+    }
+
+    aggiornareListeDadi(risultatoDado, tipoDadi){
+        if(tipoDadi == "attacco1"){
+            this.tiriAttacco1.push(risultatoDado+Number(this.modificatoreAttacco1.textContent));
+            this.tiriAttacco1 = this.ordinareListeBoubleSort(this.tiriAttacco1);
+
+        }else if(tipoDadi == "difesa1"){
+            this.tiriDifesa1.push(risultatoDado+Number(this.modificatoreDifesa2.textContent));
+            this.tiriDifesa1 = this.ordinareListeBoubleSort(this.tiriDifesa1);
+
+        }else if(tipoDadi == "attacco2"){
+            this.tiriAttacco2.push(risultatoDado+Number(this.modificatoreAttacco2.textContent));
+            this.tiriAttacco2 = this.ordinareListeBoubleSort(this.tiriAttacco2);
+
+        }else if(tipoDadi == "difesa2"){
+            this.tiriDifesa2.push(risultatoDado+Number(this.modificatoreDifesa1.textContent));
+            this.tiriDifesa2 = this.ordinareListeBoubleSort(this.tiriDifesa2);
+        }
+    }
+
+    ordinareListeBoubleSort(tiri){
+        for(let i=0; i<this.tiri.length; i++){
+            for(j=0; j<this.tiri.length-this.i-1; j++){
+                if(this.tiri[j]>this.tiri[j+1]){
+                    var valoreTemporaneo = this.tiri[j+1];
+                    this.tiri[j+1] = this.tiri[j];
+                    this.tiri[j] = valoreTemporaneo;
+                }
+            }
+        }
+
+        return this.tiri;
     }
 
     stampareInput(truppa){
         console.log("HP: " + truppa.inputHP.value + "\nDANNO: " + truppa.inputDANNO.value + "\nDIFESA: " + truppa.inputDIFESA.value + "\nATTACCO: " + truppa.inputATTACCO.value + "\nATTACCHI: " + truppa.inputATTACCHI.value + "\nMORALE: " + truppa.inputMORALE.value);
     }
+
+
+    //DADI
+    lanciaDadi(numeroDadi, tipoDadi) {
+        let durataAnimazione = 1000;
+        let durataVisibile = 2000;
+        
+        let contenitoreDadi;
+
+        if(tipoDadi == "attacco1"){
+            contenitoreDadi = document.getElementById("contenitoreDadiAttacco1");
+        }else if(tipoDadi == "difesa1"){
+            contenitoreDadi = document.getElementById("contenitoreDadiDifesa1");
+        }else if(tipoDadi == "attacco2"){
+            contenitoreDadi = document.getElementById("contenitoreDadiAttacco2");
+        }else if(tipoDadi == "difesa2"){
+            contenitoreDadi = document.getElementById("contenitoreDadiDifesa2");
+        }
+        
+        const vecchiDadi = contenitoreDadi.querySelectorAll('.dadoAttacco, .dadoDifesa');/*prendi tutti i dadi vecchi*/
+        vecchiDadi.forEach(d => d.remove());/*cancella tutti i dadi vecchi*/
+
+        const dadi = [];
+
+        // 1. Creazione dei dadi
+        for (let i = 0; i < numeroDadi; i++) {
+            const dado = document.createElement("div"); //"dado" è un nuovo elemento "generico" (è un "div")
+            if(tipoDadi == "attacco1" || tipoDadi == "attacco2"){
+                dado.classList.add("dadoAttacco"); //li dico che classe CSS deve usare il dado
+            }else if(tipoDadi == "difesa1" || tipoDadi == "difesa2"){
+                dado.classList.add("dadoDifesa"); //li dico che classe CSS deve usare il dado
+            }
+            
+            dado.textContent = "-";
+            contenitoreDadi.appendChild(dado);
+            dadi.push(dado);//aggiungo i "dado" alla lista di "dadi"
+        }
+
+        // 2. Animazione casuale veloce
+        const intervalli = dadi.map(dado => {//"intervalli" contiene tutti i "setInterval" di ogni dado             //"map" esegue un metodo su tutti gli elementi di un array
+            return setInterval(() => {//"setInterval" esegue una funzione ogni TOT tempo
+                dado.textContent = Math.floor(Math.random() * 6) + 1;
+            }, 50); //"50" è la velocità con cui viene iterato "setInterval"
+        });
+
+        // 3. Stop animazione dopo X ms
+        setTimeout(() => {
+            intervalli.forEach(interval => clearInterval(interval));// passa ogni "interval" (ID per i dadi) contenuto in "intervalli"-->lo passa a "clearInterval"-->cancella il timer
+
+            // Risultato finale
+            dadi.forEach(dado => {
+                dado.textContent = Math.floor(Math.random() * 6) + 1;
+                this.aggiornareListeDadi(Number(dado.textContent), tipoDadi);
+            });
+
+            // 4. Scomparsa dopo Y ms
+            setTimeout(() => {
+                const vecchiDadi = contenitoreDadi.querySelectorAll('.dadoAttacco, .dadoDifesa');/*prendi tutti i dadi vecchi*/
+                vecchiDadi.forEach(d => d.remove());/*cancella tutti i dadi vecchi*/
+            }, durataVisibile); // quando scatta questo tempo-->i si fa il "setTimeout()"-->i dadi vengono cancellati
+
+        }, durataAnimazione);// quando scatta questo tempo-->i si fa il "setTimeout()"-->ferma l'animazione
+    }
+
 }
 
 //Per creare la classe, sennò non c'è nulla che la crea
 const codice = new Codice();
-
-
-
-
-//DADI
-function lanciaDadi(numeroDadi, tipoDadi) {
-    durataAnimazione = 1000;
-    durataVisibile = 2000;
-    
-    let contenitoreDadi;
-
-    if(tipoDadi == "attacco1"){
-        contenitoreDadi = document.getElementById("contenitoreDadiAttacco1");
-    }else if(tipoDadi == "difesa1"){
-        contenitoreDadi = document.getElementById("contenitoreDadiDifesa1");
-    }else if(tipoDadi == "attacco2"){
-        contenitoreDadi = document.getElementById("contenitoreDadiAttacco2");
-    }else if(tipoDadi == "difesa2"){
-        contenitoreDadi = document.getElementById("contenitoreDadiDifesa2");
-    }
-    
-    const vecchiDadi = contenitoreDadi.querySelectorAll('.dadoAttacco, .dadoDifesa');/*prendi tutti i dadi vecchi*/
-    vecchiDadi.forEach(d => d.remove());/*cancella tutti i dadi vecchi*/
-
-    const dadi = [];
-
-    // 1. Creazione dei dadi
-    for (let i = 0; i < numeroDadi; i++) {
-        const dado = document.createElement("div"); //"dado" è un nuovo elemento "generico" (è un "div")
-        if(tipoDadi == "attacco1" || tipoDadi == "attacco2"){
-            dado.classList.add("dadoAttacco"); //li dico che classe CSS deve usare il dado
-        }else if(tipoDadi == "difesa1" || tipoDadi == "difesa2"){
-            dado.classList.add("dadoDifesa"); //li dico che classe CSS deve usare il dado
-        }
-        
-        dado.textContent = "-";
-        contenitoreDadi.appendChild(dado);
-        dadi.push(dado);//aggiungo i "dado" alla lista di "dadi"
-    }
-
-    // 2. Animazione casuale veloce
-    const intervalli = dadi.map(dado => {//"intervalli" contiene tutti i "setInterval" di ogni dado             //"map" esegue un metodo su tutti gli elementi di un array
-        return setInterval(() => {//"setInterval" esegue una funzione ogni TOT tempo
-            dado.textContent = Math.floor(Math.random() * 6) + 1;
-        }, 50); //"50" è la velocità con cui viene iterato "setInterval"
-    });
-
-    // 3. Stop animazione dopo X ms
-    setTimeout(() => {
-        intervalli.forEach(interval => clearInterval(interval));// passa ogni "interval" (ID per i dadi) contenuto in "intervalli"-->lo passa a "clearInterval"-->cancella il timer
-
-        // Risultato finale
-        dadi.forEach(dado => {
-            dado.textContent = Math.floor(Math.random() * 6) + 1;
-        });
-
-        // 4. Scomparsa dopo Y ms
-        setTimeout(() => {
-            const vecchiDadi = contenitoreDadi.querySelectorAll('.dadoAttacco, .dadoDifesa');/*prendi tutti i dadi vecchi*/
-            vecchiDadi.forEach(d => d.remove());/*cancella tutti i dadi vecchi*/
-        }, durataVisibile); // quando scatta questo tempo-->i si fa il "setTimeout()"-->i dadi vengono cancellati
-
-    }, durataAnimazione);// quando scatta questo tempo-->i si fa il "setTimeout()"-->ferma l'animazione
-}
